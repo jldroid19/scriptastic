f5c77e5a1b711103bdb57fccfba575d68c2f01cc
jldroid19 <admin@sandboxcsp.com>
2023-11-16 21:52:57 -0500

    offen backups

--- offen_docker_backup/one_off_backup.sh
+++ offen_docker_backup/one_off_backup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+# docker run --rm \
+#   -v data:/backup/data \
+#   --entrypoint backup \
+#   offen/docker-volume-backup:v2 --env-file ../.env
 
 docker run --rm \
   -v data:/backup/data \
-  --env AWS_ACCESS_KEY_ID="<xxx>" \
-  --env AWS_SECRET_ACCESS_KEY="<xxx>" \
-  --env AWS_S3_BUCKET_NAME="<xxx>" \
+  --env AWS_ACCESS_KEY_ID="" \
+  --env AWS_SECRET_ACCESS_KEY="" \
+  --env AWS_S3_BUCKET_NAME="" \
   --entrypoint backup \
-  offen/docker-volume-backup:v2 --env-file ../.env
+  offen/docker-volume-backup:v2
